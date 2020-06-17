eeb3c1ee86fdb683fd95a9e728eaa7d290984542
osher cohen <oscohen@localhost.localdomain>
2020-06-17 08:54:33 +0300

    MGMT-1103 disable dummy image for test mode

--- internal/bminventory/inventory.go
+++ internal/bminventory/inventory.go
@@ -74,6 +74,7 @@ type Config struct {
 	AwsAccessKeyID      string `envconfig:"AWS_ACCESS_KEY_ID" default:"accessKey1"`
 	AwsSecretAccessKey  string `envconfig:"AWS_SECRET_ACCESS_KEY" default:"verySecretKey1"`
 	Namespace           string `envconfig:"NAMESPACE" default:"assisted-installer"`
+	TestMode            bool   `envconfig:"TEST_MODE" default:"false"` // TODO remove when jobs running deprecated
 }
 
 const ignitionConfigFormat = `{
@@ -131,15 +132,18 @@ func NewBareMetalInventory(
 		debugCmdMap:   make(map[strfmt.UUID]debugCmd),
 		hostApi:       hostApi,
 		clusterApi:    clusterApi,
-		//job:           jobApi,
+		job:           jobApi,
 		eventsHandler: eventsHandler,
 		s3Client:      s3Client,
 	}
 	if cfg.ImageBuilderCmd != "" {
 		b.imageBuildCmd = strings.Split(cfg.ImageBuilderCmd, " ")
 	}
-	//	Run first ISO dummy for image pull, this is done so that the image will be pulled and the api will take less time.
-	//generateDummyISOImage(jobApi, b, log)
+
+	if !b.Config.TestMode {
+		//Run first ISO dummy for image pull, this is done so that the image will be pulled and the api will take less time.
+		generateDummyISOImage(jobApi, b, log)
+	}
 	return b
 }
 
